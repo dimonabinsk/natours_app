f92f6c90c76315b1c3081e4cbc189096cbe92078
Dmitriy Maklakov <dimonabinsk@yandex.ru>
2023-08-01 10:40:30 +0300

    рефакторинг кода

--- api/controllers/tourController.js
+++ api/controllers/tourController.js
@@ -1,5 +1,5 @@
 const Tour = require('../models/tourModel');
-
+const APIFeatures = require('../utils/apiFeatures');
 
 // промежуточное по
 exports.aliasTopTours = (req, res, next) => {
@@ -7,53 +7,17 @@ exports.aliasTopTours = (req, res, next) => {
   req.query.sort = '-ratingsAverage,price';
   req.query.fields = 'name,price,ratingsAverage,difficulty,summary';
   next();
-}
+};
 
 exports.getAllTours = async (req, res) => {
   try {
-    const queryObj = { ...req.query };
+    const features = new APIFeatures(Tour.find(), req.query)
+      .filter()
+      .sort()
+      .limitFields()
+      .paginate();
 
-    // 1) фильтрация простая
-    const excludedFields = ['page', 'sort', 'limit', 'fields'];
-    excludedFields.forEach((el) => delete queryObj[el]);
-
-    // 2) фильтрация продвинутая
-    let queryStr = JSON.stringify(queryObj);
-    queryStr = queryStr.replace(/\b(gte|gt|lte|lt)\b/g, (match) => `$${match}`);
-    // console.log(JSON.parse(queryStr));
-
-    let query = Tour.find(JSON.parse(queryStr));
-
-    // 3) Сортировка
-    if (req.query.sort) {
-      const sortBy = req.query.sort.split(',').join(' ');
-      // console.log(sortBy);
-      query = query.sort(sortBy);
-    } else {
-      // сортировка по умолчанию
-      query = query.sort('-createdAt');
-    }
-
-    // 4) Ограничение выдаваемых полей запроса
-    if (req.query.fields) {
-      const fieldsLimit = req.query.fields.split(',').join(' ');
-      query = query.select(fieldsLimit);
-    } else {
-      query = query.select('-__v');
-    }
-
-    // 5) Пагинация
-    const page = +req.query.page || 1;
-    const limit = +req.query.limit || 100;
-    const skip = (page - 1) * limit;
-    query = query.skip(skip).limit(limit);
-
-    if (req.query.page) {
-      const countTour = await Tour.countDocuments();
-      if (skip >= countTour) throw new Error('This page does not exist');
-    }
-
-    const tours = await query;
+    const tours = await features.query;
 
     res.status(200).json({
       message: 'success',
