1678ec32204adff73ae3aa45dcec924da357148c
Dmitriy Maklakov <dimonabinsk@yandex.ru>
2023-08-01 22:34:04 +0300

    добавил маршрут агрегации для сбора статистики

--- api/routes/tourRoutes.js
+++ api/routes/tourRoutes.js
@@ -7,10 +7,12 @@ const {
   updateTourId,
   deleteTourId,
   aliasTopTours,
+  getTourStats,
 } = require('../controllers/tourController');
 
 const router = express.Router();
 router.route('/top-5-cheap').get(aliasTopTours, getAllTours);
+router.route('/tour-stats').get(getTourStats);
 router.route('/').get(getAllTours).post(createTour);
 router.route('/:id').get(getTourId).patch(updateTourId).delete(deleteTourId);
 
