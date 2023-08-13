77b87a84280ea6db40d6fed48ca894ccd7ff9655
Dmitriy Maklakov <dimonabinsk@yandex.ru>
2023-08-13 21:48:23 +0300

    начал добавлять обработчики ошибок

--- api/app.js
+++ api/app.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const morgan = require('morgan');
 
+const AppError = require('./utils/appError');
+const globalErrorHandler = require('./controllers/errorController');
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
@@ -19,5 +21,22 @@ app.use((req, res, next) => {
 });
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
+
+app.all('*', (req, res, next) => {
+  // res.status(404).json({
+  //   status: 'fail',
+  //   message: `Не удалось найти адрес "${req.originalUrl}" на сервере`,
+  // });
+  // const err = new Error(
+  //   `Не удалось найти адрес "${req.originalUrl}" на сервере`,
+  // );
+  // err.status = 'fail';
+  // err.statusCode = 404;
+  next(
+    new AppError(`Не удалось найти адрес ${req.originalUrl} на сервере`, 404),
+  );
+});
+
+app.use(globalErrorHandler);
 
 module.exports = app;
