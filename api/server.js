7ecdf909c77952b1661d5cd43dc21592e96922e2
Dmitriy Maklakov <dimonabinsk@yandex.ru>
2023-08-17 19:54:34 +0300

    закончил обработку ошибок

--- api/server.js
+++ api/server.js
@@ -1,5 +1,11 @@
 const dotenv = require('dotenv');
 
+process.on('uncaughtException', (err) => {
+  console.log('НЕПЕРЕХВАЧЕННОЕ ИСКЛЮЧЕНИЕ!💥 Отключаюсь...');
+  console.log(err.name, err.message);
+  process.exit(1);
+});
+
 if (process.env.NODE_ENV === 'debug') {
   dotenv.config({ path: '../config.env' });
 }
@@ -7,22 +13,29 @@ if (process.env.NODE_ENV === 'debug') {
 const mongoose = require('mongoose');
 
 const app = require('./app');
-
+// process.env.PASSWORD
 if (process.env.DATABASE) {
   mongoose
     .connect(process.env.DATABASE, {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       user: process.env.USERNAME,
-      pass: process.env.PASSWORD,
+      pass: '12345',
       dbName: 'natours',
     })
     .then(() => {
       console.log('DB connection successful... 💯');
     });
-}
-if (process.env.PORT) {
-  app.listen(process.env.PORT, () => {
+
+  const server = app.listen(process.env.PORT, () => {
     console.log(`Приложение запущено на порте: ${process.env.PORT}...`);
+  });
+
+  process.on('unhandledRejection', (err) => {
+    console.log('НЕОБРАБОТАННЫЙ ОТКАЗ!💥 Отключаюсь...');
+    console.log(err.name, err.message);
+    server.close(() => {
+      process.exit(1);
+    });
   });
 }
