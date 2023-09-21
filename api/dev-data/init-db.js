5b2e5a12238d72b950ec5c569d676c5011a02e50
Dmitriy Maklakov <dimonabinsk@yandex.ru>
2023-09-22 00:15:43 +0300

    залил рабочие данные в БД

--- api/dev-data/init-db.js
+++ api/dev-data/init-db.js
@@ -6,8 +6,12 @@ dotenv.config({ path: '../../config.env' });
 const mongoose = require('mongoose');
 
 const Tour = require('../models/tourModel');
+const User = require('../models/userModel');
+const Review = require('../models/reviewModel');
 
 const tours = JSON.parse(fs.readFileSync(`${__dirname}/data/tours.json`));
+const users = JSON.parse(fs.readFileSync(`${__dirname}/data/users.json`));
+const reviews = JSON.parse(fs.readFileSync(`${__dirname}/data/reviews.json`));
 
 mongoose
   .connect(process.env.DATABASE, {
@@ -22,6 +26,10 @@ mongoose
 const initDB = async () => {
   try {
     await Tour.create(tours);
+    await User.create(users, {
+      validateBeforeSave: false,
+    });
+    await Review.create(reviews);
     console.log('DB init successfully');
   } catch (error) {
     console.log(error);
@@ -32,6 +40,8 @@ const initDB = async () => {
 const deleteDB = async () => {
   try {
     await Tour.deleteMany();
+    await User.deleteMany();
+    await Review.deleteMany();
     console.log('DB delete successfully');
   } catch (error) {
     console.log(error);
