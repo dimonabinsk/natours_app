7345a8be280e0a8ee80225f200dc67926742d9f7
Dmitriy Maklakov <dimonabinsk@yandex.ru>
2023-09-28 20:18:57 +0300

    создал главную страницу

--- api/controllers/viewController.js
+++ api/controllers/viewController.js
@@ -1,8 +1,17 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync(async (req, res, next) => {
+  // 1 получаем данные туров из коллекции
+  const tours = await Tour.find();
+  // 2 создаём шаблон визуализации
+
+  // 3 рендер шаблона на основе данных из 1 пункта
   res.status(200).render('overview', {
     title: 'All Tour',
+    tours,
   });
-};
+});
 
 exports.getTour = (req, res) => {
   res.status(200).render('tour', {
